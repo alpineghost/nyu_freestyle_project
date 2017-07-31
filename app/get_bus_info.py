ee8b9b88fc03fd27e227efea62ab7425f01b6e28
Jonathan Geis <jonathangeis@gmail.com>
2017-07-31 19:57:27 -0400

    print bus info on screen

--- app/get_bus_info.py
+++ app/get_bus_info.py
@@ -1,19 +1,61 @@
 
+
 import json
-import urllib2
+import urllib
+from urllib.request import urlopen
+import os
 import sys
+
+import pandas as pd
 
 
 #Set variables
-MTAkey = sys.argv[1]
-busLine = sys.argv[2]
+#MTAkey = sys.argv[1]
+#busLine = sys.argv[2]
+MTAkey = 'bbecb0d4-3937-4fd7-a4bd-4892b317c4f3'
+busLine = 'B63'
 
 url = "http://bustime.mta.info/api/siri/vehicle-monitoring.json?key=%s&VehicleMonitoringDetailLevel=calls&LineRef=%s"%(MTAkey,busLine)
 
-response = urllib2.urlopen(url)
+response = urlopen(url)
 data = response.read().decode("utf-8")
 
 dataDict = json.loads(data)
 
 vehicleInformation = dataDict['Siri']['ServiceDelivery']['VehicleMonitoringDelivery']
 numberOfBuses = len(vehicleInformation[0]['VehicleActivity'])
+
+columns = ['Latitude','Longitude','Stop Name','Stop Status']
+
+df = pd.DataFrame(columns=columns)
+
+for i in range (0,numberOfBuses):
+
+    df.loc[i,'Latitude'] = \
+    vehicleInformation[0]['VehicleActivity'][i]['MonitoredVehicleJourney']['VehicleLocation']['Latitude']
+
+    df.loc[i,'Longitude'] = \
+    vehicleInformation[0]['VehicleActivity'][i]['MonitoredVehicleJourney']['VehicleLocation']['Longitude']
+
+    onwardCallsDict = vehicleInformation[0]['VehicleActivity'][i]['MonitoredVehicleJourney']['OnwardCalls']
+
+    if (onwardCallsDict != {}):
+
+        df.loc[i,'Stop Name'] =vehicleInformation[0]['VehicleActivity'][i]['MonitoredVehicleJourney']\
+        ['OnwardCalls']['OnwardCall'][0]['StopPointName']
+
+        df.loc[i,'Stop Status'] =vehicleInformation[0]['VehicleActivity'][i]['MonitoredVehicleJourney']\
+        ['OnwardCalls']['OnwardCall'][0]['Extensions']['Distances']['PresentableDistance']
+
+    else:
+
+        df.loc[i, 'Stop Name'] = 'N/A'
+        df.loc[i, 'Stop Status'] = 'N/A'
+
+print (df)
+
+#if not len(sys.argv) == 4:
+#    print("Invalid number of arguments. Run as: python aPythonScriptThatWritesToCSV.py mycvs.csv")
+#    sys.exit()
+
+#df.to_csv(sys.argv[3], index=False)
